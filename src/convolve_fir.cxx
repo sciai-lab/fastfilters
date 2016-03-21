441aaea3b0ab12b3821502c90ce896be082c3587
Sven Peter <sven.peter@iwr.uni-heidelberg.de>
2016-03-21 17:13:30 +0100

    cur_inputconvolve_fir_inner_single

--- src/convolve_fir.cxx
+++ src/convolve_fir.cxx
@@ -60,6 +60,60 @@ void convolve_fir_inner_single(const float *input, const unsigned int n_pixels, 
     }
 }
 
+void convolve_fir_outer_single(const float *input, const unsigned int n_pixels, const unsigned n_times, float *output,
+                               const float *kernel, const unsigned int kernel_len)
+{
+    for (unsigned int i = 0; i < n_times; ++i) {
+
+        // take next line of pixels
+        const float *cur_input = input + i;
+        float *cur_output = output + i;
+
+        // left border
+        unsigned int j = 0;
+        for (j = 0; j < kernel_len / 2; ++j) {
+            float sum = 0.0;
+
+            for (unsigned int k = 0; k < kernel_len; ++k) {
+                const int kreal = k - kernel_len / 2;
+                if (kreal + (int)j < 0)
+                    sum += kernel[k] * cur_input[(j - kreal) * n_times];
+                else
+                    sum += kernel[k] * cur_input[(j + kreal) * n_times];
+            }
+
+            cur_output[j * n_times] = sum;
+        }
+
+        // full line
+        for (; j < n_pixels - kernel_len / 2; ++j) {
+            float sum = 0.0;
+
+            for (unsigned int k = 0; k < kernel_len; ++k) {
+                const int kreal = k - kernel_len / 2;
+                sum += kernel[k] * cur_input[(j + kreal) * n_times];
+            }
+
+            cur_output[j * n_times] = sum;
+        }
+
+        // right border
+        for (; j < n_pixels; ++j) {
+            float sum = 0.0;
+
+            for (unsigned int k = 0; k < kernel_len; ++k) {
+                const int kreal = k - kernel_len / 2;
+                if (kreal + j >= n_pixels)
+                    sum += kernel[k] * cur_input[(j - kreal) * n_times];
+                else
+                    sum += kernel[k] * cur_input[(j + kreal) * n_times];
+            }
+
+            cur_output[j * n_times] = sum;
+        }
+    }
+}
+
 } // namespace detail
 
 } // namespace fastfilters
