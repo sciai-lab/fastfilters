fc2f7550e514a430a08833e45cce610c2becd6b0
Sven Peter <sven.peter@iwr.uni-heidelberg.de>
2016-04-25 13:26:26 +0200

    test all 2D filters

--- tests/vigra_compare.py
+++ tests/vigra_compare.py
@@ -12,9 +12,10 @@ except ImportError:
 
 a = np.random.randn(1000000).reshape(1000,1000).astype(np.float32)
 
+sigmas = [1.0, 5.0, 10.0]
 
 for order in [0,1,2]:
-	for sigma in [1.0, 5.0, 10.0]:
+	for sigma in sigmas:
 		res_ff = ff.gaussian2d(a, order, sigma)
 		res_vigra = vigra.filters.gaussianDerivative(a, sigma, [order,order])
 
@@ -22,7 +23,7 @@ for order in [0,1,2]:
 			raise Exception("FAIL: ", order, sigma, np.max(np.abs(res_ff - res_vigra)))
 
 
-for sigma in [1.0, 5.0, 10.0]:
+for sigma in sigmas:
 	res_ff = ff.hog2d(a, sigma)
 	res_vigra = vigra.filters.hessianOfGaussianEigenvalues(a, sigma).reshape((-1,2)).swapaxes(0,1)
 	print("HOG", sigma, np.max(np.abs(res_ff - res_vigra)))
@@ -31,8 +32,26 @@ for sigma in [1.0, 5.0, 10.0]:
 		raise Exception("FAIL: HOG", sigma, np.max(np.abs(res_ff - res_vigra)))
 
 
-for sigma in [1.0, 5.0, 10.0]:
-	for sigma2 in [1.0, 5.0, 10.0]:
+for sigma in sigmas:
+	res_ff = ff.gradmag2d(a, sigma)
+	res_vigra = vigra.filters.gaussianGradientMagnitude(a, sigma)
+	print("gradmag2d ", order, sigma, np.max(np.abs(res_ff - res_vigra)))
+
+	if not np.allclose(res_ff, res_vigra, atol=1e-6):
+		raise Exception("FAIL: gradmag2d ", order, sigma, np.max(np.abs(res_ff - res_vigra)))
+
+
+for sigma in sigmas:
+	res_ff = ff.laplacian2d(a, sigma)
+	res_vigra = vigra.filters.laplacianOfGaussian(a, sigma)
+	print("laplacian2d ", order, sigma, np.max(np.abs(res_ff - res_vigra)))
+
+	if not np.allclose(res_ff, res_vigra, atol=1e-6):
+		raise Exception("FAIL: laplacian2d ", order, sigma, np.max(np.abs(res_ff - res_vigra)))
+
+
+for sigma in sigmas:
+	for sigma2 in sigmas:
 		res_ff = ff.st2d(a, sigma2, sigma)
 		res_vigra = vigra.filters.structureTensorEigenvalues(a, sigma, sigma2).reshape((-1,2)).swapaxes(0,1)
 		print("ST", sigma, sigma2, np.max(np.abs(res_ff - res_vigra)))
