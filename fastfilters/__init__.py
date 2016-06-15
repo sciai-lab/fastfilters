7fb3a15a6dc0480c83d955b6ae289d9bbe503eaf
Sven Peter <sven.peter@iwr.uni-heidelberg.de>
2016-06-15 23:12:39 +0200

    try to transpose array to make it c continuous

--- fastfilters/__init__.py
+++ fastfilters/__init__.py
@@ -1,6 +1,18 @@
 from .core import *
-__all__ = ["g"]
+import numpy as np
+
 __version__ = core.__version__
+
+def __reorder_array(array):
+	if array.flags['C_CONTIGUOUS']: return array
+
+	array = array.transpose(np.argsort(array.strides)[::-1])
+	return np.ascontiguousarray(array)
+
+def __p_fix_array(func):
+	def func_wrapper(array, *args, **kwargs):
+		return func(__reorder_array(array), *args, **kwargs)
+	return func_wrapper
 
 def __get_fn(array, fn_2d, fn_3d):
 	if hasattr(array, 'axistags'):
@@ -20,5 +32,6 @@ def __get_fn(array, fn_2d, fn_3d):
 		else: return NotImplementedError("Invalid array shape.")
 	raise NotImplementedError("Invalid array shape.")
 
+@__p_fix_array
 def gaussianSmoothing(array, sigma, window_size=0.0):
-	return __get_fn(array, gaussian2d, gaussian3d)(array, 0, sigma, window_size=window_size)
+	return __get_fn(array, gaussian2d, gaussian3d)(array, 0, sigma, window_size)
