649a6333c2b2bca3ed1a824a77deb5814b52b9ee
Sven Peter <sven.peter@iwr.uni-heidelberg.de>
2016-06-19 16:36:04 +0200

    conda: update tests

--- pkg/conda/fastfilters/run_test.sh
+++ pkg/conda/fastfilters/run_test.sh
@@ -1,9 +1,4 @@
 python ${SRC_DIR}/tests/vigra_compare.py compare-success.txt
-test -f compare-success.txt
-
 python ${SRC_DIR}/tests/vigra_compare3d.py compare-3d-success.txt
-test -f compare-3d-success.txt
-
 python ${SRC_DIR}/tests/vigra_compare_rgb.py compare-rgb-success.txt
-test -f compare-rgb-success.txt
-
+python ${SRC_DIR}/tests/border_bug.py
