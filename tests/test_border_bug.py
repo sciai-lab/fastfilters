e94992ae56299dbfc7567ff9600cd6904c8d946d
Fynn Beuttenmueller <f.beuttenmueller@gmx.de>
2016-07-25 20:31:41 +0200

    implemented automated testing as post-build event with nosetests. nosetests does not exactly do the right thing though...

---
+++ tests/test_border_bug.py
@@ -0,0 +1,14 @@
+import sys
+print("\nexecuting test file", __file__, file=sys.stderr)
+exec(compile(open('set_paths.py', "rb").read(), 'set_paths.py', 'exec'))
+import fastfilters as ff
+import numpy as np
+
+def test_border_bug():
+    a = np.ones((150,150), dtype=np.float32)
+    a[:] = np.nan
+    a[25:125, 25:125] = 0
+
+    for _ in range(10):
+        res = ff.gaussianSmoothing(a[25:125, 25:125], np.sqrt(99), window_size=10)
+        assert(np.all(res == 0))
