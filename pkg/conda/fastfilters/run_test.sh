198bf8af7475295e9fe92ae74a3663138bd0bb2a
Stuart Berg <bergs@janelia.hhmi.org>
2017-03-21 16:32:44 -0400

    conda-recipe: tests: Explicitly copy test files into the working directory for the test step, as required by newer versions of conda

--- pkg/conda/fastfilters/run_test.sh
+++ pkg/conda/fastfilters/run_test.sh
@@ -1,4 +1,4 @@
-python ${SRC_DIR}/tests/vigra_compare.py compare-success.txt
-python ${SRC_DIR}/tests/vigra_compare3d.py compare-3d-success.txt
-python ${SRC_DIR}/tests/vigra_compare_rgb.py compare-rgb-success.txt
-python ${SRC_DIR}/tests/border_bug.py
+python tests/vigra_compare.py compare-success.txt
+python tests/vigra_compare3d.py compare-3d-success.txt
+python tests/vigra_compare_rgb.py compare-rgb-success.txt
+python tests/border_bug.py
