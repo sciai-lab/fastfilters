eb270d2dcb58019993df57b2d63b6685e0a20ae9
Carsten Haubold <carsten.haubold@iwr.uni-heidelberg.de>
2017-11-06 17:13:36 +0100

    Add windows conda package build script

---
+++ pkg/conda/fastfilters/bld.bat
@@ -0,0 +1,17 @@
+mkdir build
+cd build
+
+set CONFIGURATION=Release
+
+cmake .. -G "%CMAKE_GENERATOR%" ^
+         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+         -DPYTHON_EXECUTABLE="%PYTHON%"
+
+if errorlevel 1 exit 1
+
+cmake --build . --target ALL_BUILD --config %CONFIGURATION%
+if errorlevel 1 exit 1
+
+cmake --build . --target INSTALL --config %CONFIGURATION%
+if errorlevel 1 exit 1
